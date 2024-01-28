578a64793a6515f402cd84425b2e625abb9575d3
Monish Deswal <monishdeswal2002@gmail.com>
2024-01-28 22:56:04 +0530

    last f

--- src/Notes/NOTES.jsx
+++ src/Notes/NOTES.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ProjectNote from '../Component/Project_Note/ProjectNote'
 import Note from '../Component/Note/Note'
+import styles from './NOTES.module.css'
 function NOTES() {
   return (
     <div style={{
         display:"flex"
     }}>
-        <ProjectNote/>
+        <ProjectNote />
         <Note />
     </div>
   )
